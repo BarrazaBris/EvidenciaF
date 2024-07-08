0a4b79d505001c7a0bd6725c690bbab8271f2bf4
BarrazaBris <bris.sara.barraza@gmail.com>
2024-07-08 14:32:04 -0600

    Add files via upload

---
+++ JOptionepane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Briseida
+ */
+class JOptionepane {
+
+    static void showMessage(Object object, String sobrepeso) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
